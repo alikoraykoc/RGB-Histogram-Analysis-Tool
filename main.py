de4b7658667963a6c9e3dd5f7f8d8a313404eee7
Ali Koray Koç <kocalikoray@gmail.com>
2024-08-18 00:01:17 +0300

    change minor details

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from chardet import detect
+import argparse
 
 
 def remove_brights(data, max_value):
@@ -23,11 +24,18 @@ def calculate_mean(dataframe, max_value):
     return mean_value
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", required=True, help="Input directory")
+parser.add_argument("-o", "--output", required=True, help="Output directory")
+parser.add_argument("-m", "--max-value", required=True, help="Maximum value that you want to include to the "
+                                                             "calculations")
+args = parser.parse_args()
+
 output_name = []
 output_mean = []
-directory = "histogram_data"
+directory = str(args.input)
 files = Path(directory).glob('*')
-mx_value = int(input("Please enter the maximum value that you want to include to mean calculation: "))
+mx_value = int(args.max_value)
 for file in files:
     if "/." in file.name or file.name[0] == ".":
         continue
@@ -40,15 +48,16 @@ for file in files:
     except KeyError:
         print(file.name)
     file = str(file)
-    output_name.append(file[15:-4])
+    output_name.append(file.split("/")[-1].split(".")[0])
     output_mean.append(mean)
 
 dictionary = {
     "individual": output_name,
-    "color_mean": output_mean,
+    "mean": output_mean,
 }
 output_df = pd.DataFrame(dictionary)
 output_df.sort_values(by="individual", inplace=True)
-output_df.to_excel("results/output.xlsx")
+output_directory = str(args.output)
+output_df.to_excel(f"{output_directory}/results.xlsx")
 print("Done! Note: Please move the output excel file to another directory before rerunning the script otherwise it may "
       "override the previous results.")
